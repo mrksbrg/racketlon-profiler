2a3a33254ac0f9f91134fb48e71c43888e37602d
Markus Borg <markus.borg@codescene.com>
2023-07-25 11:04:25 +0200

    Add Leiningen project structure
    Using app template

---
+++ src/racketlon_profiler/core.clj
@@ -0,0 +1,7 @@
+(ns racketlon-profiler.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/racketlon_profiler/core_test.clj
@@ -0,0 +1,7 @@
+(ns racketlon-profiler.core-test
+  (:require [clojure.test :refer :all]
+            [racketlon-profiler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
